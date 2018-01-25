4fabac57f74dc4926309967e067c2928cc90173c
arfu2016 <ffnn_ustc@163.com>
2018-01-25 23:09:50 +0800

    Update 3sum.py

--- Python/3sum.py
+++ Python/3sum.py
@@ -13,6 +13,37 @@
 #    A solution set is:
 #    (-1, 0, 1)
 #    (-1, -1, 2)
+
+class Solution:
+    def three_sum(self, nums):
+        nums = sorted(nums)
+        i = 0
+        result = []
+        while i < len(nums)-2:
+            if i == 0 or nums[i] > nums[i-1]:
+                j = i+1
+                k = len(nums)-1
+                while j<k:
+                    if nums[i]+nums[j]+nums[k] < 0:
+                        j+= 1
+                    elif nums[i]+nums[j]+nums[k] > 0:
+                        k-= 1
+                    else:
+                        result.append([nums[i], nums[j], nums[k]])
+                        j+= 1
+                        k-= 1
+                    while j<k and nums[j] == nums[j-1]:
+                        j+= 1
+                    while j<k and nums[k] == nums[k+1]:
+                        k- =1
+                        
+            i+= 1
+            return result
+# time: O(n^2)
+# space: O(n)
+
+
+
 import collections
 
 
